877e20e4b4ee71b3d2030801dbe4ddd754941173
Rayy0527 <1668834312@qq.com>
2024-01-15 17:59:56 +0800

    Add include/hero_chassis_controller.h:Kinematics Inverse.

--- include/hero_chassis_controller/hero_chassis_controller.h
+++ include/hero_chassis_controller/hero_chassis_controller.h
@@ -5,9 +5,71 @@
 #ifndef HERO_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
 #define HERO_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
 
+
+#define ENCODER_RESOLUTION      1440.0   //编码器分辨率, 轮子转一圈，编码器产生的脉冲数
+#define WHEEL_DIAMETER          0.1525    //轮子直径,单位：米
+#define D_X                     0.18     //底盘Y轴上两轮中心的间距
+#define D_Y                     0.25     //底盘X轴上两轮中心的间距
+#define PID_RATE                50       //PID调节PWM值的频率
+
 #include <ros/ros.h>
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
+class KinematicsInverse
+{
+private:
+    double pulse_per_meter = 0;
+    float rx_plus_ry_cali = 0.3;
+    double angular_correction_factor = 1.0;
+    double linear_correction_factor = 1.0;
+
+public:
+    /**
+  * @函数作用：运动学解析参数初始化
+  */
+    void Kinematics_Init(void)
+    {
+        pulse_per_meter = (float)(ENCODER_RESOLUTION/(WHEEL_DIAMETER*3.1415926))/linear_correction_factor;
+
+        float r_x = D_X/2;
+        float r_y = D_Y/2;
+        rx_plus_ry_cali = (r_x + r_y)/angular_correction_factor;
+    }
+
+/**
+  * @函数作用：逆向运动学解析，底盘三轴速度-->轮子速度
+  * @输入：机器人三轴速度 m/s
+  * @输出：电机应达到的目标速度（一个PID控制周期内，电机编码器计数值的变化）
+  */
+    void Kinematics_Inverse(int16_t* input, int16_t* output)
+    {
+        float r_x = D_X/2;
+        float r_y = D_Y/2;
+        float v_tx   = (float)input[0];
+        float v_ty   = (float)input[1];
+        float omega = (float)input[2];
+        static float v_w[4] = {0};
+
+        v_w[0] = v_tx - v_ty - (r_x + r_y)*omega;
+        v_w[1] = v_tx + v_ty + (r_x + r_y)*omega;
+        v_w[2] = v_tx + v_ty - (r_x + r_y)*omega;
+        v_w[3] = v_tx - v_ty + (r_x + r_y)*omega;
+
+        output[0] = (int16_t)(v_w[0] * pulse_per_meter/PID_RATE);
+        output[1] = (int16_t)(v_w[1] * pulse_per_meter/PID_RATE);
+        output[2] = (int16_t)(v_w[2] * pulse_per_meter/PID_RATE);
+        output[3] = (int16_t)(v_w[3] * pulse_per_meter/PID_RATE);
+    }
+};
+
+/*Kinematics Forward正运动学*/
+
+#define ENCODER_MAX 32767
+#define ENCODER_MIN -32768
+#define ENCODER_LOW_WRAP  ((ENCODER_MAX - ENCODER_MIN)*0.3+ENCODER_MIN)
+#define ENCODER_HIGH_WRAP ((ENCODER_MAX - ENCODER_MIN)*0.7+ENCODER_MIN)
+#define PI 3.1415926
+
 
 namespace hero_chassis_controller {
 
